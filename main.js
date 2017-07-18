529841141a16cba0ea78f61375bf5fb299c169ea
Megha Bhargava <er.meghabhargava@gmail.com>
2017-07-17 22:28:02 -0700

    modification in music app

--- main.js
+++ main.js
@@ -1,44 +1,40 @@
-var currentSongNumber = 1;
-var willLoop = 0;
-var willShuffle = 0;
-
-
-
-
-
+//---------------loop-------------------
+		var currentSongNumber = 1;
+		var willLoop = 0;
+		var willShuffle = 0;
 
 
 //----------------------songs details...array of objects----------------------
-var songs = [{
-        'name': 'Badri Ki Dulhania (Title Track)',
-        'artist': 'Neha Kakkar, Monali Thakur, Ikka Singh, Dev Negi',
-        'album': 'Badrinath ki Dulhania',
-        'duration': '2:56',
-       'fileName': 'song1.mp3',
-	   'image': 'song1.jpg'
+  var songs = [{
+	'name': 'Apne_Apne',
+        'artist': 'Sonu Nigam, Jayesh Gandhi, Jaspinder Narula',
+        'album': 'Apne',
+        'duration': '6:48',
+        'fileName': 'song/song1.mp3',
+		'image': 'song1.jpg'
     },
     {
-        'name': 'Humma Song',
-        'artist': 'Badshah, Jubin Nautiyal, Shashaa Tirupati',
-        'album': 'Ok Jaanu',
-        'duration': '3:15',
-        'fileName': 'song2.mp3',
-		'image': 'song2.jpg'
+        'name': 'Kal ho naa ho',
+        'artist': 'Sonu Nigam',
+        'album': 'Kal ho naa ho',
+        'duration': '5:21',
+       'fileName': 'song/song2.mp3',
+	   'image': 'song2.jpg'
     },
     {
-        'name': 'Nashe Si Chadh Gayi',
-        'artist': 'Arijit Singh',
-        'album': 'Befikre',
-        'duration': '2:34',
-        'fileName': 'song3.mp3',
+        'name': 'Purani Jeans',
+        'artist': 'Ali Haider',
+        'album': 'Mahi',
+        'duration': '5:06',
+        'fileName': 'song/song3.mp3',
 		'image': 'song3.jpg'
     },
     {
-        'name': 'The Breakup Song',
-        'artist': 'Nakash Aziz, Arijit Singh, Badshah, Jonita Gandhi',
-        'album': 'Ae Dil Hai Mushkil',
-        'duration': '2:29',
-        'fileName': 'song4.mp3',
+        'name': 'Right Now Now',
+        'album': 'Housefull2',
+        'artist': 'Sunidhi Chauhan, Suzanne D Mello,Wajid Ali',
+        'duration': '4:06',
+        'fileName': 'song/song4.mp3',
 		'image': 'song4.jpg'
     }]
 
@@ -53,10 +49,7 @@ $('.welcome-screen button').on('click', function() {
             $('#name-input').addClass('error');
         }
     });
-	//----------------loop for repeat icon--------------------
-	
-	
-	
+
 	function toggleSong(){
 		var song = document.querySelector('audio');
 		if(song.paused == true) {
@@ -70,6 +63,16 @@ $('.welcome-screen button').on('click', function() {
 		song.pause();
 		}
 	} 
+	
+	//-------------progress bar-------------------
+	function UpdateTimer()
+	{
+		var song = document.querySelector('audio');
+		var currentTime = song.currentTime;
+		var duration = song.duration;
+		var percentage = (currentTime/duration)*100;
+		$('.progress-filled').css('width', percentage+ "%");
+		}
 	
 	//--------------fancy time format---------------------------
 	
@@ -97,10 +100,18 @@ $('.welcome-screen button').on('click', function() {
 		var currentTime = Math.floor(song.currentTime);
 		currentTime = fancyTimeFormat(currentTime);
 		var duration = Math.floor(song.duration);
+		var per= currentTime/duration * 100;
 		duration = fancyTimeFormat(duration);
 		$('.time-elapsed').text(currentTime);
 		$('.song-duration').text(duration);
+		
 	}
+	//---------- time jump function to the end of the song by -5 & play next song--------------
+	function timeJump() {
+    var song = document.querySelector('audio')
+    song.currentTime = song.duration - 5;
+}
+	
 	
 	//-------- song calling function-------------
 	function addSongNameClickEvent(songObj,position) {
@@ -121,15 +132,11 @@ $('.welcome-screen button').on('click', function() {
 			});
 			}
 			
-			
-			
-			
-			
+	//------------- deatils of the song-------------	
 	function changeCurrentSongDetails(songObj) {
     $('.current-song-image').attr('src','img/' + songObj.image)
     $('.current-song-name').text(songObj.name)
     $('.current-song-album').text(songObj.album)
-
 	}
 	 
 	window.onload = function() {
@@ -140,13 +147,14 @@ $('.welcome-screen button').on('click', function() {
 		
 		updateCurrentTime();
 		},1000);
-		//var songList = ['Badri Ki Dulhania (Title Track)','Humma Song', 'Nashe Si Chadh Gayi', 'The Breakup Song'];
-		//var fileNames = ['song1.mp3','song2.mp3','song3.mp3','song4.mp3'];
-		//var artistList = ['Neha Kakkar, Monali Thakur, Ikka Singh, Dev Negi','Badshah, Jubin Nautiyal, Shashaa Tirupati','Arijit Singh','Nakash Aziz, Arijit Singh, Badshah, Jonita Gandhi'];
-		//var albumList = ['Badrinath ki Dulhania','Ok Jaanu','Befikre','Ae Dil Hai Mushkil'];
-		//var durationList = ['2:56','3:15','2:34','2:29'];
-
+		//----------time duration bar--------------
+		setInterval(function() {
 		
+		UpdateTimer();
+		},1000);
+		
+		
+	
 		
 		
 		for(var i =0; i < songs.length;i++) {
@@ -161,24 +169,51 @@ $('.welcome-screen button').on('click', function() {
 		}
 		
 		
-		function move() {
-    var elem = document.getElementById("myBar"); 
-    var width = 1;
-    var id = setInterval(frame, 10);
-    function frame() {
-        if (width >= 100) {
-            clearInterval(id);
-        } else {
-            width++; 
-            elem.style.width = width + '%'; 
-        }
-    }
-}
+		$('.fa-repeat').on('click',function() {
+			$('.fa-repeat').toggleClass('disabled')
+			willLoop = 1 - willLoop;
+			});	
+			
+			//---------------shuffle icon code-------------									
+			$('.fa-random').on('click',function() {
+			$('.fa-random').toggleClass('disabled');
+			willShuffle = 1 - willShuffle;
+			});	
+		//--------------------- end event-----------------------------				
+			$('audio').on('ended',function() {
+			var audio = document.querySelector('audio');
+			if (willShuffle == 1) {
+			var nextSongNumber = randomExcluded(1,4,currentSongNumber); // Calling our function from Stackoverflow
+			var nextSongobj = songs[nextSongNumber-1];
+			audio.src = nextSongobj.fileName;
+			toggleSong();
+			changeCurrentSongDetails(nextSongobj);
+			currentSongNumber = nextSongNumber;
+			}
+			else if(currentSongNumber < 4) {
+			var nextSongobj = songs[currentSongNumber];
+			audio.src = nextSongobj.fileName;
+			toggleSong();
+			changeCurrentSongDetails(nextSongobj);
+			currentSongNumber = currentSongNumber + 1;
+			}
+			else if(willLoop == 1) {
+			var nextSongobj = songs[0];
+			audio.src = nextSongobj.fileName;
+			toggleSong();
+			changeCurrentSongDetails(nextSongobj);
+			currentSongNumber =  1;
+			}
+			else {
+			$('.play-icon').removeClass('fa-pause').addClass('fa-play');
+			audio.currentTime = 0;
+			}
+			});
+		
+		
+		
 
-function timeJump() {
-    var song = document.querySelector('audio')
-    song.currentTime = song.duration - 5;
-}
+
 $('audio').on('ended',function() {
     var audio = document.querySelector('audio');
     if(currentSongNumber < 4) {
@@ -188,54 +223,71 @@ $('audio').on('ended',function() {
       console.log('stop');  // Stop Playing
     }
 })
+  //---------------forward function-----------------
+		$('.fa-step-forward').on('click', function(){
+			var audio=document.querySelector('audio');
+			if(currentSongNumber < 4){
+				var nextSongobj = songs[currentSongNumber];
+				audio.src = nextSongobj.fileName;
+				toggleSong();
+				changeCurrentSongDetails(nextSongobj);
+				currentSongNumber=currentSongNumber + 1;
+				console.log('nextSong');
+			}
+			else{
+				audio.currentTime = 0;
+				console.log('nextSong');
+			}
+		});
 
-$('.fa-repeat').on('click',function() {
-    $('.fa-repeat').toggleClass('disabled')
-    willLoop = 1 - willLoop;
-});
-
-$('.fa-random').on('click',function() {
-    $('.fa-random').toggleClass('disabled')
-    willShuffle = 1 - willShuffle;
-});
-		
-		// for(var i =0; i < songList.length;i++) {
-			//	var name = '#song' + (i+1);
-				//var song = $(name);
-				//song.find('.song-name').text(songList[i]);
-				//song.find('.song-artist').text(artistList[i]);
-				//song.find('.song-album').text(albumList[i]); // Added
-				//song.find('.song-length').text(durationList[i]); // Added
-			//}
-	//var songName1 = 'Tamma Song';
-	 //var songName2 = 'Humma Song';
-	//var songName3 = 'Nashe Si Chadh Gayi';
-	//var songName4 = 'The Breakup Song'; 
-			//$('#song1 .song-name').text(songList[0]);
-		//$('#song2 .song-name').text(songList[1]);
-		//$('#song3 .song-name').text(songList[2]);
-		//$('#song4 .song-name').text(songList[3]);
-		
-		//--------------artist list--------------
-		//$('#song1 .song-artist').text(artistList[0]);
-    //$('#song2 .song-artist').text(artistList[1]);
-   // $('#song3 .song-artist').text(artistList[2]);
-    //$('#song4 .song-artist').text(artistList[3]);
+	//------------backward function--------------
 	
-		//------------ song calling event--------------
-		//for (var i = 0; i < fileNames.length ; i++) {
-		//	addSongNameClickEvent(fileNames[i],i+1)
-		//}
+	$('.fa-step-backward').on('click', function(){
+			var audio=document.querySelector('audio');
+			if(currentSongNumber < 4){
+				var nextSongobj = songs[currentSongNumber];
+				audio.src = nextSongobj.fileName;
+				toggleSong();
+				changeCurrentSongDetails(nextSongobj);
+				currentSongNumber = currentSongNumber - 1;
+				console.log('previous song');
+			}
+			else{
+				audio.currentTime = 0;
+				console.log('previous song');
+			}
+		});
+
+
+
+
+
+
+
+
+
+
+
+
 		
-			//addSongNameClickEvent(fileNames[0],1);
-			//addSongNameClickEvent(fileNames[1],2);
-			//addSongNameClickEvent(fileNames[2],3);
-			//addSongNameClickEvent(fileNames[3],4);
+		
+		
+		
+		
+		
+		
+		
+		
 	 	//-------------------------------------datatables	
 			$('#songs').DataTable({
         paging: false
     });
-			
+		
+
+		
+
+
+		
 			
 		}
 	
--- piano/main.js
+++ piano/main.js
@@ -1,206 +1,516 @@
-window.onload= function() {
-	
+
+ window.onload= function() 
+ {
 	var piano = Synth.createInstrument('piano');
+	//var acoustic = Synth.createInstrument('acoustic');
+	
 
-		//-------------------------q-------------
-		$("#key_1") .on('click',function(){
+
+	//----------------WHITE KEYS CODE---------------------
+	//------------------------Q-------------------
+		
+	$("#key_1") .on('click',function()
+		{
 			piano.play('B',4,2);
 		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode == 81){
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode == 81 || event.keyCode==113)
+		{
 			piano.play('B',4,2);
 			
 		}
 		});
-		//----------------------------w-------------
-		$("#key_2") .on('click',function(){
+		
+	//---------------------W-------------
+		
+	$("#key_2") .on('click',function()
+		{
 			piano.play('C',4,2);
 		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==87){
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==87 || event.keyCode==119)
+		{
 			piano.play('C',4,2);
 		}
 		});
-		//----------------------------e------------------
-		$("#key_3") .on('click',function(){
+		
+	//-------------------------E------------------
+		
+	$("#key_3") .on('click',function()
+		{
 			piano.play('F',4,2);
 		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==69){
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==69 || event.keyCode==101)
+		{
 			piano.play('F',4,2);
 		}
 		});
-		//-------------------------------r---------
+		
+	//----------------------R--------------
 
-		$("#key_4") .on('click',function(){
+	$("#key_4") .on('click',function()
+		{
 			piano.play('A',4,2);
 		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==82){
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==82 || event.keyCode==114)
+		{
 			piano.play('A',4,2);
 		}
 		});
-	//-----------------t-----------
-		$("#key_5") .on('click',function(){
+		
+	//-----------------T-----------
+	
+	$("#key_5") .on('click',function()
+		{
 			piano.play('B',4,2);
 		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==84){
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==84 || event.keyCode==116)
+		{
 			piano.play('B',4,2);
 		}
 		});
-		//----------------------y--------------
-		$("#key_6") .on('click',function(){
+		
+	//----------------------Y--------------
+		
+	$("#key_6") .on('click',function()
+		{
 			piano.play('D',4,2);
 		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==89){
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==89 || event.keyCode==121)
+		{
 			piano.play('D',4,2);
 		}
 		});
-		//------------------u--------------------
-		$("#key_7") .on('click',function(){
+		
+	//------------------U--------------------
+		
+	$("#key_7") .on('click',function()
+		{
 			piano.play('A',4,2);
 		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==85){
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==85 || event.keyCode==117)
+		{
 			piano.play('A',4,2);
 		}
 		});
-		//----------------------i-------------------
-		$("#key_8") .on('click',function(){
+		
+	//----------------------I-------------------
+		
+	$("#key_8") .on('click',function()
+		{
 			piano.play('F',4,2);
 		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==73){
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==73 || event.keyCode==105)
+		{
 			piano.play('F',4,2);
 		}
 		});
-		//---------------------o---------------------
-		$("#key_9") .on('click',function(){
+		
+	//---------------------O---------------------
+		
+	$("#key_9") .on('click',function()
+		{
 			piano.play('B',4,2);
 		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==79){
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==79 || event.keyCode==111)
+		{
 			piano.play('B',4,2);
 		}
 		});
-		//---------------------------p------------------
+		
+	//---------------------------P------------------
 
-		$("#key_10") .on('click',function(){
+	$("#key_10") .on('click',function()
+		{
 			piano.play('C',4,2);
 		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==80){
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==80 || event.keyCode==112)
+		{
 			piano.play('C',4,2);
 		}
 		});
-		//-----------------[---------------------
-		$("#key_11") .on('click',function(){
+		
+	//-----------------[---------------------
+		
+	$("#key_11") .on('click',function()
+		{
 			piano.play('F',4,2);
 		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==91){
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==91)
+		{
 			piano.play('F',4,2);
 		}
 		});
-		//----------------------]------------
+		
+	//----------------------]------------
 
-		$("#key_12") .on('click',function(){
+	$("#key_12") .on('click',function()
+		{
+			piano.play('G',4,2);
+		});
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==93)
+		{
+			piano.play('G',4,2);
+		}
+		});
+		
+	//--------------Z-------------------
+		
+	$("#key_13") .on('click',function()
+		{
+			piano.play('A',4,2);
+		});
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==90 || event.keyCode==112)
+		{
+			piano.play('A',4,2);
+		}
+		});
+		
+	//---------------------X-----------------------
+		
+	$("#key_14") .on('click',function()
+		{
+			piano.play('C',4,2);
+		});
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==88 || event.keyCode==120)
+		{
+			piano.play('C',4,2);
+		}
+		});
+		
+	//------------------------C-----------------
+		
+	$("#key_15") .on('click',function()
+		{
 			piano.play('F',4,2);
 		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==221){
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==67 || event.keyCode==99)
+		{
 			piano.play('F',4,2);
 		}
 		});
-		//--------------Z-------------------
-		$("#key_13") .on('click',function(){
-			piano.play('F',4,2);
+		
+	//-----------------------------V-------------------
+		
+	$("#key_16") .on('click',function()
+		{
+			piano.play('D',4,2);
 		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==90){
-			piano.play('F',4,2);
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==86 || event.keyCode==118)
+		{
+			piano.play('D',4,2);
 		}
 		});
-		//---------------------X-----------------------
-		$("#key_14") .on('click',function(){
-			piano.play('F',4,2);
+		
+	//---------------------------B---------------
+		
+	$("#key_17") .on('click',function()
+		{
+			piano.play('E',4,2);
 		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==88){
-			piano.play('F',4,2);
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==66 || event.keyCode==98)
+		{
+			piano.play('E',4,2);
 		}
 		});
-		//------------------------C-----------------
-		$("#key_15") .on('click',function(){
-			piano.play('F',4,2);
+		
+	//---------------------------N--------------------
+		
+	$("#key_18") .on('click',function()
+		{
+			piano.play('B',4,2);
 		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==67){
-			piano.play('F',4,2);
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==78 || event.keyCode==110)
+		{
+			piano.play('B',4,2);
 		}
 		});
-		//-----------------------------V-------------------
-		$("#key_16") .on('click',function(){
-			piano.play('F',4,2);
+		
+	//----------------------M------------------
+		
+	$("#key_19") .on('click',function()
+		{
+			piano.play('C',4,2);
 		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==86){
-			piano.play('F',4,2);
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==77 || event.keyCode==109)
+		{
+			piano.play('C',4,2);
 		}
 		});
-		//---------------------------B---------------
-		$("#key_17") .on('click',function(){
-			piano.play('F',4,2);
+		
+	//------------------------------,-----------------
+		
+	$("#key_20") .on('click',function()
+		{
+			piano.play('A',4,2);
 		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==66){
-			piano.play('F',4,2);
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==44)
+		{
+			piano.play('A',4,2);
 		}
 		});
-		//---------------------------N--------------------
-		$("#key_18") .on('click',function(){
-			piano.play('F',4,2);
+		
+	//--------------------------------.---------------------
+		
+	$("#key_21") .on('click',function()
+		{
+			piano.play('C',4,2);
 		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==78){
-			piano.play('F',4,2);
-		}
-		});
-		//----------------------M------------------
-		$("#key_19") .on('click',function(){
-			piano.play('F',4,2);
-		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==77){
-			
-			console.log('hjeuk');
-			piano.play('F',4,2);
-		}
-		});
-		//------------------------------,-----------------
-		$("#key_20") .on('click',function(){
-			
-			console.log(event.keycode);
-			piano.play('F',4,2);
-		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==188){
-			
-			piano.play('F',4,2);
-		}
-		});
-		//--------------------------------.---------------------
-		$("#key_21") .on('click',function(){
-			piano.play('F',4,2);
-		});
-		$("body") .on('keypress',function(event){
-		if(event.keyCode==190){
-			piano.play('F',4,2);
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==46)
+		{
+			piano.play('C',4,2);
 		}
 		});
 	
-}
- 
+	
+	//----------------------black keys code---------------
+	
+	//----------------2-----------
+	
+	$("#key-1") .on('click',function()
+		{
+			piano.play('F#',4,2);
+		});
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==50)
+		{
+			piano.play('F#',4,2);
+		}
+		});
+	
+	//-----------------3---------
+	
+	$("#key-2") .on('click',function()
+		{
+			piano.play('C#',4,2);
+		});
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==51)
+		{
+			piano.play('C#',4,2);
+		}
+		});
+	
+	//------------------5--------------
+	
+	$("#key-3") .on('click',function()
+		{
+			piano.play('D#',4,2);
+		});
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==53)
+		{
+			piano.play('D#',4,2);
+		}
+		});
+	
+	//------------------6----------------
+	
+	$("#key-4") .on('click',function()
+		{
+			piano.play('A#',4,2);
+		});
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==54)
+		{
+			piano.play('A#',4,2);
+		}
+		});
+	
+	//-----------------7----------------
+	
+	$("#key-5") .on('click',function()
+		{
+			piano.play('F#',4,2);
+		});
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==55)
+		{
+			piano.play('F#',4,2);
+		}
+		});
+	
+	//-------------------9-------------------
+	
+	$("#key-6") .on('click',function()
+		{
+			piano.play('G#',4,2);
+		});
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==57)
+		{			
+			piano.play('G#',4,2);
+		}
+		});
+	
+	//----------------0-----------
+	
+	$("#key-7") .on('click',function()
+		{
+			piano.play('F#',4,2);
+		});
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==48)
+		{
+			piano.play('F#',4,2);
+		}
+		});
+	
+	//-----------------=------------
+	
+	$("#key-8") .on('click',function()
+		{
+			piano.play('D#',4,2);
+		});
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==61){
+			piano.play('D#',4,2);
+		}
+		});
 		
+	//-----------------A----------
+	
+	$("#key-9") .on('click',function()
+		{
+			piano.play('C#',4,2);
+		});
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==65 || event.keyCode==97)
+		{
+			piano.play('C#',4,2);
+		}
+		});
+		
+	//-----------------S-----------------
+	
+	$("#key-10") .on('click',function()
+		{
+			piano.play('F#',4,2);
+		});
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==83 || event.keyCode==115)
+		{
+			piano.play('F#',4,2);
+		}
+		});
+		
+	//----------------F-------------------
+		
+		$("#key-11") .on('click',function()
+		{
+			piano.play('D#',4,2);
+		});
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==70 || event.keyCode==102)
+		{
+			piano.play('D#',4,2);
+		}
+		});
+		
+	//-----------------G-----------------
+		
+		$("#key-12") .on('click',function()
+		{
+			piano.play('A#',4,2);
+		});
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==71 || event.keyCode==103)
+		{
+			piano.play('A#',4,2);
+		}
+		});
+		
+	//-----------------J-----------
+		
+		$("#key-13") .on('click',function()
+		{
+			piano.play('G#',4,2);
+		});
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==74 || event.keyCode==106)
+		{
+			piano.play('G#',4,2);
+		}
+		});
+		
+	//----------------K---------------
+		
+		$("#key-14") .on('click',function()
+		{
+			piano.play('F#',4,2);
+		});
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==75 || event.keyCode==107)
+		{
+			piano.play('F#',4,2);
+		}
+		});
+		
+	//----------------L-----------------
+		
+		$("#key-15") .on('click',function()
+		{
+			piano.play('A#',4,2);
+		});
+		$("body") .on('keypress',function(event)
+		{
+		if(event.keyCode==76 || event.keyCode==108)
+		{
+			piano.play('A#',4,2);
+		}
+		});
+ }
